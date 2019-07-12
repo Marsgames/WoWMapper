6d4f0fd50ed625bee3bb737c9d2e10ac459c85ea
Raphael Daumas <raphdaumas@hotmail.fr>
2019-07-12 15:37:08 +0200

    Exemple librairie

---
+++ WM_Lib.lua
@@ -0,0 +1,36 @@
+
+-- Nom / Faction
+local capitalCity = {
+   "Orgrimmar",
+   "Fossoyeuse",
+   "Hurlevent"
+}
+
+-- Nom / Lieu (description compréhensive)
+local aubergistOrgri = {
+   "aze",
+   "rty",
+   "uiop"
+}
+
+-- Nom / Lieu
+local flyOrgri = {
+   "le petit bonhomme vers les tours"
+}
+
+
+local boatOrgri = {
+   "Mulgore",
+   "Toundra Boréenne",
+   "Strangleronce",
+   "Fossoyeuse"
+}
+
+local Orgri = 
+{
+   aubergistOrgri,
+   flyOrgri,
+   boatOrgri
+}
+
+capitalCity.Orgrimmar = Orgri
