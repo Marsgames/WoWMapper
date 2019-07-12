be16bda82e8ecb2e325dd5b8b8e39f6d1971a573
Raphael Daumas <raphdaumas@hotmail.fr>
2019-07-12 16:14:16 +0200

    Update WM_Lib.lua

--- WM_Lib.lua
+++ WM_Lib.lua
@@ -1,36 +1,23 @@
 
--- Nom / Faction
-local capitalCity = {
-   "Orgrimmar",
-   "Fossoyeuse",
-   "Hurlevent"
-}
 
--- Nom / Lieu (description compréhensive)
-local aubergistOrgri = {
-   "aze",
-   "rty",
-   "uiop"
-}
+local capitalCity = {}
 
--- Nom / Lieu
-local flyOrgri = {
-   "le petit bonhomme vers les tours"
-}
+capitalCity.Orgrimmar = {}
+capitalCity.Orgrimmar["Name"] = "Orgrimmar"
+capitalCity.Orgrimmar["Affiliation"] = "Horde"
+capitalCity.Orgrimmar["Location"] = "Kalimdor"
+capitalCity.Orgrimmar["Races"] = {"Orcs", "Trolls ?", "Gobelins ?", "Morts vivants ?"}
+capitalCity.Orgrimmar["Ruler"] = "Sylvanas"
+capitalCity.Orgrimmar["Description"] = "Orgrimmar est la capitale de la Horde. Située au nord de la région de Durotar, en Kalimdor. On trouve de nombreux services et quêtes dans cette ville, ainsi que des Hôtel des ventes, une banque, des marchands d'animaux de compagnie, et des Maîtres pour tous les Métiers et toutes les Classes. En tant que capitale des Orcs, Orgrimmar est le lieu de résidence de Varok Saurcroc et abrite de nombreux quartiers où demeurent toutes les populations de la Horde."
+
+capitalCity.Stormwind = {}
+capitalCity.Stormwind["Name"] = "Hurlevent"
+capitalCity.Stormwind["Affiliation"] = "Alliance"
+capitalCity.Stormwind["Location"] = "Royaumes de l'Est"
+capitalCity.Stormwind["Races"] = {"Humains"}
+capitalCity.Stormwind["Ruler"] = "Anduin Wrynn"
+capitalCity.Stormwind["Description"] = "La cité de Hurlevent est le plus important bastion des humains en Azeroth, située dans la forêt d'Elwynn. Après la Seconde guerre, Hurlevent est une merveille d'architecture et de maçonnerie humaines. Les gardes assurent la sécurité à l'intérieur des murs de la ville, et le roi Varian Wrynn dirige son peuple du haut de son imposant donjon. Alors que le commerce bat son plein dans les rues du quartier commerçant, les aventuriers de toutes sortes se promènent dans les rues de la Vieille Ville, survivante du pillage de la cité lors de la Seconde guerre. Non affectée par les ravages du Fléau au nord, Hurlevent doit faire face à différentes menaces venant de l'intérieur comme de l'extérieur.
+Hurlevent est la dernière grande ville humaine à avoir été reconstruite. Elle s'est proclamée ultime place-forte de l'humanité, après l'invasion de la Légion ardente au nord des Royaumes de l'est. Avec Lordaeron entre les mains des Réprouvés, et de nombreuses villes en ruine, Hurlevent reste un lieu de commerce où les membres de l'Alliance sont en sécurité.
+L'entrée de Hurlevent a été conçue pour rendre hommage aux héros morts durant la guerre. Des statues au nom de différents héros flanquent l'accès. Toute personne qui emprunte ce chemin, nommé la Vallée des Héros, ne peut que se sentir fière d'appartenir à l'Alliance."
 
 
-local boatOrgri = {
-   "Mulgore",
-   "Toundra Boréenne",
-   "Strangleronce",
-   "Fossoyeuse"
-}
-
-local Orgri = 
-{
-   aubergistOrgri,
-   flyOrgri,
-   boatOrgri
-}
-
-capitalCity.Orgrimmar = Orgri
